28edc2489191c1efa7ded8098f6d47417aee67c8
一闪一闪亮晶晶 <resetsix223@gmail.com>
2024-04-06 17:49:04 +0800

    chore: add proxy config

--- config/craco.config.cjs
+++ config/craco.config.cjs
@@ -1,1 +1,14 @@
-module.exports = {};
+module.exports = {
+	// devServer: {
+	// 	proxy: {
+	// 		"/api": {
+	// 			// 此处配置需要代理的 API 接口地址。如 http://host:port
+	// 			target: "http://xxx",
+	// 			changeOrigin: true,
+	// 			pathRewrite: {
+	// 				"^/api": "",
+	// 			},
+	// 		},
+	// 	},
+	// },
+};
