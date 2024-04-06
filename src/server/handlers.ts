e3db587488e8fe668d826f35d4a8f0d14fe0cf14
一闪一闪亮晶晶 <resetsix223@gmail.com>
2024-04-06 17:54:47 +0800

    fix: modified login logic

--- src/server/handlers.ts
+++ src/server/handlers.ts
@@ -11,15 +11,23 @@ export const handlers = [
 		const data: any = await request.json();
 		console.log("data", data);
 		const { username, password } = data;
-		if (isEmpty(username || password)) {
-			await delay(800);
-			return HttpResponse.json({ code: "400", message: "用户名或密码不能为空" });
+		if (isEmpty(username) && isEmpty(password)) {
+			await delay(600);
+			return HttpResponse.json({ code: "400", message: "用户名和密码都不能为空" });
+		}
+		if (isEmpty(username)) {
+			await delay(600);
+			return HttpResponse.json({ code: "400", message: "用户名不能为空" });
+		}
+		if (isEmpty(password)) {
+			await delay(600);
+			return HttpResponse.json({ code: "400", message: "密码不能为空" });
 		}
 		if (username === _username && password === _passwords) {
-			await delay(800);
+			await delay(600);
 			return HttpResponse.json({ code: "200", message: "登录成功！" });
 		} else {
-			await delay(800);
+			await delay(600);
 			return HttpResponse.json({ code: "400", message: "用户名或密码错误" });
 		}
 	}),
